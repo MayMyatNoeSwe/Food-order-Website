6fc933a72bfc6f24cbb4506c8ee723a2f67ada9c
MayMyatNoeSwe <myatnoen395@gmail.com>
2025-01-20 11:28:48 +0630

    part-2

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,51 @@
-<html>
-<title>Food Order </title>
+<?php include('partials/menu.php') ?>
+<!-- Main Content Section Starts -->
+<div class="main-content">
+    <div class="wrapper">
+        <h1>DASHBOARD</h1>
+        <div class="col-4 text-center">
+            <h1>5</h1>
+            <br>
+            Categories
+        </div>
+        <div class="col-4 text-center">
+            <h1>5</h1>
+            <br>
+            Categories
+        </div>
+        <div class="col-4 text-center">
+            <h1>5</h1>
+            <br>
+            Categories
+        </div>
+        <div class="col-4 text-center">
+            <h1>5</h1>
+            <br>
+            Categories
+        </div>
+        <!-- <div class="col-4 text-center">
+                <h1>5</h1>
+                <br>
+                Categories
+            </div> -->
+        <div class="clearfix">
+
+        </div>
+    </div>
+</div>
+<!-- Main Content Section Starts -->
+
+<!-- Footer Section Starts -->
+<div class="footer">
+    <div class="wrapper">
+        <p class="text-center">
+            2020 All rights reserved, Some Restaurant. Developed By - <a href="#">Vijay Thapa</a>
+        </p>
+    </div>
+
+
+</div>
+<!-- Footer Section Starts -->
+</body>
 
 </html>
---
+++ admin/manage-admin.php
@@ -0,0 +1,47 @@
+<html>
+
+<head>
+
+    <title>Food Order Website-Home Page </title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+
+<body>
+    <!-- Menu Section Starts -->
+    <div class="menu text-center">
+        <div class="wrapper">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Admin</a></li>
+                <li><a href="#">Category</a></li>
+                <li><a href="#">Food</a></li>
+                <li><a href="#">Order</a></li>
+            </ul>
+
+        </div>
+    </div>
+    <!-- Menu Section Ends -->
+
+    <!-- Main Content Section Starts -->
+    <div class="main-content">
+        <div class="wrapper">
+            <h1>Manage Admin</h1>
+
+        </div>
+    </div>
+    <!-- Main Content Section Starts -->
+
+    <!-- Footer Section Starts -->
+    <div class="footer">
+        <div class="wrapper">
+            <p class="text-center">
+                2020 All rights reserved, Some Restaurant. Developed By - <a href="#">Vijay Thapa</a>
+            </p>
+        </div>
+
+
+    </div>
+    <!-- Footer Section Starts -->
+</body>
+
+</html>
